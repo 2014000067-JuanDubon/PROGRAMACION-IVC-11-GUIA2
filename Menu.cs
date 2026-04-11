cf5254731743f2a8f322b112426cbf4622513120
2014000067-JuanDubon <j.dubon.5adb@gmail.com>
2026-04-11 16:11:41 -0600

    Add files via upload

---
+++ Menu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int opc;
+
+        Console.WriteLine("1");
+        Console.WriteLine("2");
+        Console.WriteLine("3");
+        Console.WriteLine("4");
+
+        Console.Write("Opcion: ");
+        opc = int.Parse(Console.ReadLine());
+
+        switch (opc)
+        {
+            case 1: Console.WriteLine("Hamburguesa"); break;
+            case 2: Console.WriteLine("Pizza"); break;
+            case 3: Console.WriteLine("Pollo"); break;
+            case 4: Console.WriteLine("Ensalada"); break;
+            default: Console.WriteLine("Error"); break;
+        }
+        Console.ReadKey();
+    }
+}
+}
---
+++ Menu.psc
@@ -0,0 +1,20 @@
+Algoritmo Menu
+	Definir opc Como Entero
+	
+	Escribir "1 Hamburguesa"
+	Escribir "2 Pizza"
+	Escribir "3 Pollo"
+	Escribir "4 Ensalada"
+	
+	Escribir Sin Saltar "Opcion: "
+	Leer opc
+	
+	Segun opc Hacer
+		1: Escribir "Hamburguesa"
+		2: Escribir "Pizza"
+		3: Escribir "Pollo"
+		4: Escribir "Ensalada"
+		De Otro Modo:
+			Escribir "Error"
+	FinSegun
+FinAlgoritmo
